a99eaf0d314262dc1d12f18c90c58a086904e6a3
sangeun <tkddms31@direa.co.kr>
2022-02-22 15:46:23 +0900

    정리중

--- src/directive/permission/index.js
+++
@@ -1,7 +0,0 @@
-import permission from './permission'
-
-if (window.Vue) {
-  window['permission'] = permission
-}
-
-export default permission
--- src/directive/permission/permission.js
+++
@@ -1,7 +0,0 @@
-
-export default {
-  inserted(el, binding) {
-  },
-  update(el, binding) {
-  }
-}
